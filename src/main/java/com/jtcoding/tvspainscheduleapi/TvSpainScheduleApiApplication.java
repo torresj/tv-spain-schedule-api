c1c960bf379bb52994a07f6948a96f1d8402573c
Jaime Torres <jtbenavente@gmail.com>
2023-07-22 13:46:15 +0200

    First structure

---
+++ src/main/java/com/jtcoding/tvspainscheduleapi/TvSpainScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.jtcoding.tvspainscheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvSpainScheduleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvSpainScheduleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtcoding/tvspainscheduleapi/TvSpainScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtcoding.tvspainscheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvSpainScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
